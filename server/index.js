e22c14000874f9564ae8bd1eaa29bb8ee8cc92e5
Davide Murroni <davi.murroni@gmail.com>
2024-05-04 18:41:11 +0200

    added get

--- server/index.js
+++ server/index.js
@@ -19,3 +19,7 @@ app.use("/api", require("./routes/api"));
 
 // Start server
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.get("/", (req, res) => {
+  res.json("hello");
+});
